3573018ec3875470ba38752564facb23aa86985c
Chelsea Crooke <chelsea.crooke@yahoo.com>
2019-11-10 14:31:44 -0500

    timer is half working, all questions will appear, issues with the images showing up again

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,160 +1,159 @@
 $(document).ready(function () {
-        // Question, answer, image arrays
-        var questions = [
-            {
-                 question: "Which of the following IS a color visible in the visible light spectrum?",
-                 options: ["White" , "Teal" , "Red" , "Pink"],
-                 correctAnswer: 2,
-                 image: "<img src='assets/images/question1.webp'>"
-                },
-            {
-                 question: "All of the following are types of electromagnetic radiation EXCEPT (       ).",
-                 options: ["X-rays" , "Ultraviolet Light" , "Infared Light" , "Florescent Light"],
-                 correctAnswer: 3,
-                 image: "<img src='assets/images/question2.jpeg'>"
-                },
-            {
-                 question: "Which color has the longest wavelength?",
-                 options: ["Red" , "Blue" , "Violet" , "Yellow"],
-                 correctAnswer: 0,
-                 image: "<img src='assets/images/question3.webp'>"
-                },
-             {
-                 question: "(Blank) rays are used to kill bacteria and other microbes. ",
-                 options: ["X-rays" , "Infared Rays" , "Ultraviolet Rays" , "Gamma Rays"],
-                 correctAnswer: 3,
-                 image: "<img src='assets/images/question4.webp'>"
-                },
-            {
-                 question: "Who discovered radio waves?",
-                 options: ["Thomas Edison" , "Heinrich Hertz" , "Alexander Graham Bell" , "Edwin Hubble"],
-                 correctAnswer: 1,
-                 image: "<img src='assets/images/question5.jpeg'>"
-                },
-            {
-                 question: "Which color has the shortest wavelength?",
-                 options: ["Violet" , "Red" , "Blue" ,"Yellow"],
-                 correctAnswer: 0,
-                 image: "<img src='assets/images/question6.webp'>"
-                },
-            {
-                 question: "Which type of electromagnetic wave has the longest wavelength?",
-                 options: ["Gamma waves" , "Radio Waves" , "Microwaves" , "Visible Light Waves"],
-                 correctAnswer: 1,
-                 image: "<img src='assets/images/question7.webp'>"
-                },
-            {
-                 question: "Who discovered X-rays?",
-                 options: ["Wilhelm Roentgen" , "Albert Einstein" , "Isaac Newton" , "Nikola Tesla"],
-                 correctAnswer: 0, 
-                 image: "<img src='assets/images/question8.jpeg'>"
-                },
-            {
-                 question: "(       ) is used in cameras for night vision and in medical treatments.", 
-                 options: ["Ultraviolet Rays" , "X-rays" , "Gamma Rays" , "Infared Rays"],
-                 correctAnswer: 3,
-                 image: "<img src='assets/images/question9.jpeg'>"
-                },
-            {
-                 question: "The microwave was accidentally created during WWII from radar technology.",
-                 options: ["True" , "False"],
-                 correctAnswer: 0,
-                 image: "<img src='assets/images/question10.webp'>"
-                },
-        ]
-    
-        console.log(questions);
+    var card = $("#questionBox");
+    var countStartNum = 30;
+            // Questions, answer, image arrays//
+    var questions = [{
+        question: "Which of the following IS a color visible in the visible light spectrum?",
+        options: ["White" , "Teal" , "Red" , "Pink"],
+        correctAnswer: "Red",
+        image: "<img src='../images/quest1.jpeg'>"
+        }, {
+        question: "All of the following are types of electromagnetic radiation EXCEPT (________).",
+        options: ["X-rays" , "Ultraviolet Light" , "Infared Light" , "Florescent Light"],
+        correctAnswer: "Florescent Light",
+        image: "assets/images/question2.jpeg"
+        },  {
+        question: "Which color has the longest wavelength?",
+        options: ["Red" , "Blue" , "Violet" , "Yellow"],
+        correctAnswer: "Red",
+        image: "assets/images/quest3.html"
+        }, {
+        question: "(______) rays are used to kill bacteria and other microbes. ",
+        options: ["X-rays" , "Infared Rays" , "Ultraviolet Rays" , "Gamma Rays"],
+        correctAnswer: "Gamma Rays",
+        image: "assets/images/quest4.jpeg",
+        }, {
+        question: "Who discovered radio waves?",
+        options: ["Thomas Edison" , "Heinrich Hertz" , "Alexander Graham Bell" , "Edwin Hubble"],
+        correctAnswer: "Heinrich Hertz",
+        image: "assets/images/question5.jpeg",
+        }, {
+        question: "Which color has the shortest wavelength?",
+        options: ["Violet" , "Red" , "Blue" ,"Yellow"],
+        correctAnswer: "Violet",
+        image: "assets/images/quest6.gif"
+        }, {
+        question: "Which type of electromagnetic wave has the longest wavelength?",
+        options: ["Gamma waves" , "Radio Waves" , "Microwaves" , "Visible Light Waves"],
+        correctAnswer: "Radio Waves",
+        image: "assets/images/quest7.html"
+        },  {
+        question: "Who discovered X-rays?",
+        options: ["Wilhelm Roentgen" , "Albert Einstein" , "Isaac Newton" , "Nikola Tesla"],
+        correctAnswer: "Wilheml Roentgen", 
+        image: "assets/images/question8.jpeg"
+        },  {
+        question: "(_____) is used in cameras for night vision and in medical treatments.", 
+        options: ["Ultraviolet Rays" , "X-rays" , "Gamma Rays" , "Infared Rays"],
+        correctAnswer: "Infared Rays",
+        image: "assets/images/question9.jpeg"
+        }, {
+        question: "The microwave was accidentally created during WWII from radar technology.",
+        options: ["True" , "False"],
+        correctAnswer: "True",
+        image: "assets/images/quest10.html"
+        }];
+    // Var to hold setInterval//
+    var timer;
+    var game = {
+        questions: questions,
+        currentQuest: 0,
+        counter: countStartNum,
+        correct: 0,
+        wrong: 0,
 
- var correct= 0;
- var incorrect= 0;
- var unansweredCount= 0;
- var timer= 45;
- var intervalId;
- var userGuesses="";
- var running= false;
- var questionCounter= questions.length;
- var pick;
- var index;
- var newArray= [];
- var holder= [];
-
- // Hide the reset button
- $("#resetButton").hide(); 
-
- // Click start button to begin game
- $("#startButton").on("click" , function (){
-     $("#startButton").hide();
-     displayQuestion ();
-     runTimer();
-     for(var i = 0; i < questions.length; i++){
-         holder.push(questions[i]);
-     }
-     $("#starimg").hide();
- });
- console.log(questions.length);
- // Start timer
-function runTimer(){
-    if(!running){
-        intervalId= setTimeout(decrement, 2000);
-        running= true;
-        runTimer.text("timeBox: " + runTimer);
-    }
-}
-// Timer countdown
-function decrement() {
-    $("#timerBox").html("<h3>Time left: " + timer + "</h3>");
-    timer--;
-
-    //Stop timer if it reaches 0
-    if(timer === 0){
-        unansweredCount++;
-        stop ();
-        $("#questionBox").html("<p> OOOHHHH Times Up! The correct answer is: " + pick.options[pick.correctAnswer] + "</p>");
+        countDown: function(){
+            this.counter--;
+            $("#timerSpot").text(this.counter);
+            if(this.counter === 0){
+                console.log("Times Up!");
+                this.timesup();
+            }
+        },
+        loadQuestion: function(){
+            timer= setInterval(this.countDown.bind(this), 1000);
+            card.html("<h3>" + questions[this.currentQuest].question + "</h3>");
+            for(var i=0; i < questions[this.currentQuest].options.length; i++){
+                card.append("<button class= 'answer-btn' 'data-name'" + questions[this.currentQuest].options[i] + "'>" + questions[this.currentQuest].options[i] + "</button>");
+            }
+        },
+        nextQuestion: function(){
+            this.counter = window.countStartNum;
+            $("#counter-num").text(this.counter);
+            this.currentQuest ++;
+            this.loadQuestion.bind(this)();
+        },
+        timeUp: function(){
+            clearInterval(window.timer);
+            $("#counter-num").text(this.counter);
+            card.html("<h3> You're out of time!</h3>");
+            card.append("<h4> The correct answer is: " + questions[this.currentQuest].correctAnswer + "</h4>");
+            card.append(questions[this.currentQuest].image
+            );
+            if(this.currentQuest === questions.length - 1){
+                setTimeout(this.results, 3 * 1000);
+            }else{
+                setTimeout(this.nextQuestion, 3 * 1000);
+            }
+        },
+        results: function(){
+            clearInterval(window.timer);
+            card.html("<h3> All done, here's your stats </h3>");
+            $("#counter-num").text(this.counter);
+            card.append("<h4> Correct Answers: " + this.correct + "</h4>");
+            card.append("<h4> Wrong Answers: " + this.wrong + "</h4>");
+            card.append("<h4> Unanswered: " + (questions.length - (this.wrong + this.correct)) + "</h4>");
+            card.append("<br><button id= 'start-over'> Try again? </button>");
+        },
+        clicked: function(e){
+            clearInterval(window.timer);
+            if($(e.target).attr("data-name") === questions[this.currentQuest].correctAnswer){
+                this.answeredRight();
+            }else{
+                this.answeredWrong();
+            }
+        },
+        answeredWrong: function(){
+            this.wrong ++;
+            clearInterval(window.timer);
+            card.html("<h3>MMM... Not really </h3>");
+            card.append("<h3> The right answer was: " + questions[this.currentQuest].correctAnswer + "</h3>");
+            card.append(questions[this.currentQuest].image);
+            if(this.currentQuest === questions.length -1){
+                setTimeout(this.results.bind(this), 3 * 1000);
+            }else{
+                setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+            }
+        },
+        answeredRight: function(){
+            this.correct ++;
+            clearInterval(window.timer);
+            card.html("<h3>YAY! Way to go, you smart!!</h3>");
+            card.append(questions[this.currentQuest].image);
+            if(this.currentQuest === questions.length - 1){
+                setTimeout(this.results.bind(this), 3 * 1000);
+            }else{
+                setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+            }
+        },
+        reset: function(){
+            this.currentQuest= 0;
+            this.counter= countStartNum;
+            this.correct= 0;
+            this.wrong= 0;
+            this.loadQuestion();
+        }
     };
-}
-// Stop timer
-function stop (){
-    running= false;
-    clearInterval(intervalId);
-}   
-// To randomly pick a question in questions array if not already shown, 
-//display question and loop through all questions and display possible answers
-function displayQuestion (){
-    index= Math.floor(Math.random() * questions.length);
-    pick= questions[index];
-
-    console.log(index);
-    console.log(pick);
-    
-    $("#questionBox").html("<h2>" + pick.question + "</h2>");
-    for(var i = 0; i < pick.question.length; i++){
-        var userGuesses= $("<div>");
-        userGuesses.addClass("options");
-        userGuesses.html(pick.options[i]);
-        userGuesses.attr("data-guessvalue", i);
-        $("#questionBox").append(userGuesses);
-    };
-}
-// Selecting answer on clicks ect
-$(".answerButton").on("click" , function(){
-    userGuesses= parseInt($(this).attr("data-guessvalue"));
-    if(userGuesses === pick.correctAnswer){
-        stop();
-        correct++;
-        userGuesses= "";
-        hidepicture ();
-        $("#answerBox").html("<p>Correct!</p>");
-    }else{
-        stop();
-        incorrect++;
-        userGuesses="";
-        hidepicture ();
-        $("#answerBox").html("<p>Nope not that one. The correct answer is: " + pick.options[pick.correctAnswer] + "</p>");
-    }
-    console.log(userGuesses);
+// Click Events //
+    $(document).on("click", "#resetButton", game.reset.bind(game));
+    $(document).on("click", ".answer-btn", function(e){
+        game.clicked.bind(game, e)();
+    });
+    $(document).on("click", "#startButton", function(){
+        $("#questionBox").prepend("<h2> Time left: <span id= 'counter-num'>30</span>seconds</h2>");
+        game.loadQuestion.bind(game)();
+        $("#starting").hide();
+        $("#startButton").hide();
+        $("#resetButton").hide();
+    });
 });
-
-});
-
-    
-   
