9fded117a230283ba7a18c4b1feeee1380a3993c
Chelsea Crooke <chelsea.crooke@yahoo.com>
2019-11-11 11:46:00 -0500

    timer issue fixed and removed images

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -6,53 +6,43 @@ $(document).ready(function () {
         question: "Which of the following IS a color visible in the visible light spectrum?",
         options: ["White" , "Teal" , "Red" , "Pink"],
         correctAnswer: "Red",
-        image: "<img src='../images/quest1.jpeg'>"
         }, {
         question: "All of the following are types of electromagnetic radiation EXCEPT (________).",
         options: ["X-rays" , "Ultraviolet Light" , "Infared Light" , "Florescent Light"],
         correctAnswer: "Florescent Light",
-        image: "assets/images/question2.jpeg"
         },  {
         question: "Which color has the longest wavelength?",
         options: ["Red" , "Blue" , "Violet" , "Yellow"],
         correctAnswer: "Red",
-        image: "assets/images/quest3.html"
         }, {
         question: "(______) rays are used to kill bacteria and other microbes. ",
         options: ["X-rays" , "Infared Rays" , "Ultraviolet Rays" , "Gamma Rays"],
         correctAnswer: "Gamma Rays",
-        image: "assets/images/quest4.jpeg",
         }, {
         question: "Who discovered radio waves?",
         options: ["Thomas Edison" , "Heinrich Hertz" , "Alexander Graham Bell" , "Edwin Hubble"],
         correctAnswer: "Heinrich Hertz",
-        image: "assets/images/question5.jpeg",
         }, {
         question: "Which color has the shortest wavelength?",
         options: ["Violet" , "Red" , "Blue" ,"Yellow"],
         correctAnswer: "Violet",
-        image: "assets/images/quest6.gif"
         }, {
         question: "Which type of electromagnetic wave has the longest wavelength?",
         options: ["Gamma waves" , "Radio Waves" , "Microwaves" , "Visible Light Waves"],
         correctAnswer: "Radio Waves",
-        image: "assets/images/quest7.html"
         },  {
         question: "Who discovered X-rays?",
         options: ["Wilhelm Roentgen" , "Albert Einstein" , "Isaac Newton" , "Nikola Tesla"],
         correctAnswer: "Wilheml Roentgen", 
-        image: "assets/images/question8.jpeg"
         },  {
         question: "(_____) is used in cameras for night vision and in medical treatments.", 
         options: ["Ultraviolet Rays" , "X-rays" , "Gamma Rays" , "Infared Rays"],
         correctAnswer: "Infared Rays",
-        image: "assets/images/question9.jpeg"
         }, {
         question: "The microwave was accidentally created during WWII from radar technology.",
         options: ["True" , "False"],
         correctAnswer: "True",
-        image: "assets/images/quest10.html"
-        }];
+     }];
     // Var to hold setInterval//
     var timer;
     var game = {
@@ -61,13 +51,14 @@ $(document).ready(function () {
         counter: countStartNum,
         correct: 0,
         wrong: 0,
+        unanswered: 0,
 
         countDown: function(){
             game.counter--;
             $("#timerSpot").text(game.counter);
-            if(this.counter === 0){
-                console.log("Times Up!");
-                this.timesup();
+            if(game.counter === 0){
+                clearInterval(timer);
+                game.timesup();
             }
         },
         loadQuestion: function(){
@@ -76,28 +67,23 @@ $(document).ready(function () {
             card.html("<h3>" + questions[this.currentQuest].question + "</h3>");
 
             for(var i=0; i < questions[this.currentQuest].options.length; i++){
-                card.append("<button class= 'answer-btn' 'data-name='" + questions[this.currentQuest].options[i] + "'>" + questions[this.currentQuest].options[i] + "</button>");
+                card.append("<button class= 'answer-btn' id='button' data-name='" + questions[this.currentQuest].options[i] + "'>" + questions[this.currentQuest].options[i] + "</button>");
             }
         },
         nextQuestion: function(){
             game.counter = countStartNum;
             $("#counter-num").text(game.counter);
-            game.currentQuest ++;
+            game.currentQuest++;
             game.loadQuestion();
         },
         timeUp: function(){
-
             clearInterval(timer);
-
             $("#counter-num").html(game.counter);
-
             card.html("<h3> You're out of time!</h3>");
-            card.append("<h4> The correct answer is: " + questions[this.currentQuest].correctAnswer);
-            card.append(questions[this.currentQuest].image + "</h4>");
-
-            if(game.currentQuest === questions.length - 1){
+            card.append("<h4> The correct answer was: " + questions[this.currentQuest].correctAnswer+ "</h4>");
+            if(game.currentQuest === questions.length - 1) {
                 setTimeout(game.results, 3 * 1000);
-            }else{
+            }else {
                 setTimeout(game.nextQuestion, 3 * 1000);
             }
         },
@@ -126,6 +112,17 @@ $(document).ready(function () {
                 this.answeredWrong();
             }
         },
+        
+        answeredRight: function(){
+            clearInterval(timer);
+            game.correct++;
+            card.html("<h3>YAY! Way to go, you smart!!</h3>");
+            if(game.currentQuest === questions.length - 1){
+                setTimeout(game.results, 3 * 1000);
+            }else{
+                setTimeout(game.nextQuestion, 3 * 1000);
+            }
+        },
         answeredWrong: function(){
 
             game.wrong++;
@@ -134,24 +131,7 @@ $(document).ready(function () {
 
             card.html("<h3>MMM... Not really </h3>");
             card.append("<h3> The right answer was: " + questions[game.currentQuest].correctAnswer + "</h3>");
-            card.append(questions[game.currentQuest].image);
-
             if(game.currentQuest === questions.length -1){
-                setTimeout(game.results, 3 * 1000);
-            }else{
-                setTimeout(game.nextQuestion, 3 * 1000);
-            }
-        },
-        answeredRight: function(){
-
-            game.correct++;
-
-            clearInterval(timer);
-
-            card.html("<h3>YAY! Way to go, you smart!!</h3>");
-            card.append(questions[game.currentQuest].image);
-
-            if(game.currentQuest === questions.length - 1){
                 setTimeout(game.results, 3 * 1000);
             }else{
                 setTimeout(game.nextQuestion, 3 * 1000);
@@ -162,6 +142,7 @@ $(document).ready(function () {
             this.counter= countStartNum;
             this.correct= 0;
             this.wrong= 0;
+            this.unanswered= 0;
             this.loadQuestion();
         }
     };
@@ -175,7 +156,7 @@ $(document).ready(function () {
     });
 
     $(document).on("click", "#startButton", function(){
-        $("#questionBox").prepend("<h2> Time left: <span id= 'counter-num'>30</span>seconds</h2>");
+        $("#questionBox").prepend("<h2> Time left: <span id='counter-num'>30</span>seconds</h2>");
         game.loadQuestion();
         $("#starting").hide();
         $("#startButton").hide();
